ca689038f7dbd9810effb74db0c317c89c45a9ea
A. Costa Brunes <brunes.alice62@gmail.com>
2025-05-23 12:11:32 -0300

    Sofri, mas deu certo

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from '../pages/HomePage/HomePage';
-import ProductsPage from '../pages/ProductsPage/ProductsPage';
-import ProductDetailPage from '../pages/ProductDetailPage/ProductDetailPage';
-import CartPage from '../pages/CartPage/CartPage';
+import FinalizarCompra from '../pages/FinalizarCompra/FinalizarCompra';
+
+
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/produtos" element={<ProductsPage />} />
-      <Route path="/produtos/:id" element={<ProductDetailPage />} />
-      <Route path="/carrinho" element={<CartPage />} />
-      {/* Adicione outras rotas conforme necessário */}
-      <Route path="*" element={<h1 className="text-center mt-5">Página não encontrada</h1>} />
+      <Route path="/FinalizarCompra" element={<FinalizarCompra />} />
+
     </Routes>
   );
 };
